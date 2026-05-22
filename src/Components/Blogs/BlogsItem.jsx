b00691d4cd96d96a53940db476bc0c53bd050b05
Jerin John <jerinjohn@Jerins-Air.lan>
2026-05-22 04:22:03 -0700

    update in blogs page

--- src/Components/Blogs/BlogsItem.jsx
+++ src/Components/Blogs/BlogsItem.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
-import { Link } from "react-router-dom";
-// import images from '../../Assets/Blogs/'; // Adjust path
 
-function BlogsItem(props) {
+function BlogsItem({ blog }) {
   return (
     <Card className="blog-card-view">
-      <Card.Img variant="top" src={"/assets/Blogs/" + props.blog.imgUrl} alt="card-img" width="200" height="200"/>
+      <Card.Img variant="top" src={"/assets/Blogs/" + blog.imgUrl} alt="card-img" width="200" height="200" />
       <Card.Body>
-        <Card.Title> {props.blog.title}</Card.Title>
-        <Card.Subtitle>{props.blog.date}</Card.Subtitle>
+        <Card.Title>{blog.title}</Card.Title>
+        <Card.Subtitle>{blog.date}</Card.Subtitle>
         <Card.Text style={{ textAlign: "justify" }}>
-          {props.blog.snippet}
-          <Card.Link>
-            <Link to={`/blogs/${props.blog.id}`} className="purple">Read More</Link>
-          </Card.Link>
+          {blog.snippet}
         </Card.Text>
-       
+        {blog.url && (
+          <a href={blog.url} target="_blank" rel="noreferrer" className="purple">
+            Read on Medium →
+          </a>
+        )}
       </Card.Body>
     </Card>
   );
