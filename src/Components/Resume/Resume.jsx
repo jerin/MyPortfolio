e349ce5f46ed5a7c8c2e9b815772f115dbae5fd2
Jerin John <jerinjohn@Jerins-Air.lan>
2026-05-21 19:24:59 -0700

    resume page upfated

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle.jsx";
-import pdf from "../../Assets/Jerin_John_Resume_ReactJS.pdf";
+import pdf from "../../assets/Jerin_John_Resume_ReactJS.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
