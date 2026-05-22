4fd4953bd88dd2be584466cf37db95640fa8284e
Jerin John <jerinjohn@Jerins-Air.lan>
2026-05-21 19:20:48 -0700

    resume page upfated

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle.jsx";
-import pdf from "../../assets/Jerin_John_Resume_ReactJS.pdf";
+import pdf from "../../Assets/Jerin_John_Resume_ReactJS.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 function Resume() {
@@ -30,24 +32,12 @@ function Resume() {
             &nbsp;Download CV
           </Button>
         </Row>
-{/* 
+
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-        </Row> */}
-
-        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row> */}
+        </Row>
       </Container>
     </div>
   );
